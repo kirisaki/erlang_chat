628e390ec3bd3c140a7dd93529cbccd7aa3e0afb
Akihito Kirisaki <kirisaki@klaraworks.net>
2018-01-26 18:01:03 +0900

    Add function to make a thread.

--- apps/erlang_chat/src/chat_controller.erl
+++ apps/erlang_chat/src/chat_controller.erl
@@ -19,35 +19,57 @@
           supervisor,
           [chat_thread_sup]}).
 
--record(state, {sup, threads, users}).
--record(user, {name, pid}).
+-record(state, {thread_sup}).
+-record(user, {name, pid, threads}).
+-record(thread, {name, pid, ref}).
 
 start_link(Sup) ->
     gen_server:start_link({local, chat_controller}, ?MODULE, {Sup}, []).
 
 init({Sup}) ->
     ets:new(users, [set, named_table, {keypos,#user.name}, protected]),
+    ets:new(threads, [set, named_table, {keypos,#thread.name}, protected]),
     self() ! {start_thread_supervisor, Sup},
-    {ok, #state{threads=dict:new()}}.
+    {ok, #state{}}.
 
 handle_call({request, Raw}, {From, _Ref}, State) ->
     io:format(Raw),
+    User_ = ets:select(users, [{#user{pid=From, _='_'}, [], ['$_']}]),
+    [User] = case length(User_) of
+                 0 -> 
+                     [#user{name=""}];
+                 _ ->
+                     User_
+             end,
     case statement_converter:decode(Raw) of
         {ok, {<<"connect">>, Name}} ->
             case ets:member(users, Name) of
                 true ->
                     {reply,{<<"error">>, <<Name/binary, " is already used.">>} , State};
                 _ ->   
-                    ets:insert(users, #user{name=Name, pid=From}),
+                    ets:insert(users, #user{name=Name, pid=From, threads=[]}),
                     {reply,{<<"welcome">>, Name} , State}
             end;
-        {ok, {<<"quit">>, Name}} ->
+        {ok, {<<"quit">>}} ->
+            Name = User#user.name,
             case ets:member(users, Name) of
                 true ->
                     ets:delete(users, Name),
                     {reply,{<<"goodbye">>, <<Name/binary>>} , State};
                 _ ->   
-                    {reply,{<<"error">>, <<Name/binary, "doesn't exist.">>} , State}
+                    {reply,{<<"error">>, <<Name/binary, " doesn't exist.">>} , State}
+            end;
+        {ok, {<<"join">>, Thread}} ->
+            case ets:member(threads, Thread) of
+                true ->
+                    ets:update_element(users, User#user.name, [{#user.threads, [Thread|User#user.threads]}]),
+                    {reply,{<<"welcome">>, <<"thread ", Thread/binary>>} , State};
+                _ ->   
+                    {ok, Pid} = supervisor:start_child(State#state.thread_sup, [Thread]),
+                    Ref = erlang:monitor(process, Pid),
+                    ets:update_element(users, User#user.name, [{#user.threads, [Thread|User#user.threads]}]),
+                    ets:insert(threads, #thread{name=Thread, pid=Pid, ref=Ref}),
+                    {reply,{<<"welcome">>, <<"thread ", Thread/binary, " has made.">>} , State}
             end;
         _ ->
             {reply,{<<"error">>, <<"invalide statement.">>} , State}
@@ -55,16 +77,6 @@ handle_call({request, Raw}, {From, _Ref}, State) ->
 handle_call(_Request, _From, State) ->
     {reply, ignored, State}.
 
-handle_cast({<<"nick">>, Id, Name}, State) ->
-    Res = supervisor:start_child(State#state.sup,[Name, Id]),
-    case Res of
-        {ok, Pid} ->
-            Ref = erlang:monitor(process, Pid),
-            {noreply, State};
-        Error ->
-            error(Error),
-            {noreply, State}
-    end;
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
@@ -72,7 +84,7 @@ handle_info({start_thread_supervisor, Sup}, State) ->
     Res = supervisor:start_child(Sup, ?SUP_SPEC),
     case Res of
         {ok, Pid} ->
-            {noreply, State#state{sup = Pid}};
+            {noreply, State#state{thread_sup = Pid}};
         {_, Error} ->
             {noreply, State}
     end;
--- apps/erlang_chat/src/chat_thread.erl
+++ apps/erlang_chat/src/chat_thread.erl
@@ -1,7 +1,7 @@
 -module(chat_thread).
 
 -behaviour(gen_server).
--export([start_link/2]).
+-export([start_link/1]).
 
 %% gen_server callbacks
 -export([init/1,
@@ -11,14 +11,14 @@
          terminate/2,
          code_change/3]).
 
--record(state, {name, id}).
+-record(state, {name}).
 
-start_link(Name, Id) ->
-    gen_server:start_link(?MODULE, [Name, Id],[]).
+start_link(Name) ->
+    gen_server:start_link(?MODULE, [Name],[]).
 
-init([Name, Id]) ->
-    io:format("thread ~s ~s start", [Id, Name]),
-    {ok, #state{name=Name, id=Id}}.
+init([Name]) ->
+    io:format("thread ~s start", [Name]),
+    {ok, #state{name=Name}}.
 
 handle_call({request, Raw}, {From, _Ref}, State) ->
     {reply, ok, State};
