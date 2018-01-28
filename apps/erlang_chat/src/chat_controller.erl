8b37454fa9e91fab62c1972b54437626c35b8aee
Akihito KIRISAKI <kirisaki@klaraworks.net>
2018-01-28 21:04:31 +0900

    Move function of handle statement to ws_handler.

--- apps/erlang_chat/src/chat_controller.erl
+++ apps/erlang_chat/src/chat_controller.erl
@@ -27,53 +27,27 @@ start_link(Sup) ->
     gen_server:start_link({local, chat_controller}, ?MODULE, {Sup}, []).
 
 init({Sup}) ->
-    ets:new(users, [set, named_table, {keypos,#user.name}, protected]),
-    ets:new(threads, [set, named_table, {keypos,#thread.name}, protected]),
+    ets:new(users, [set, named_table, {keypos,#user.name}, public]),
+    ets:new(threads, [set, named_table, {keypos,#thread.name}, public]),
     self() ! {start_thread_supervisor, Sup},
     {ok, #state{}}.
 
-handle_call({request, Raw}, {From, _Ref}, State) ->
-    io:format(Raw),
-    User_ = ets:select(users, [{#user{pid=From, _='_'}, [], ['$_']}]),
-    [User] = case length(User_) of
-                 0 -> 
-                     [#user{name=""}];
-                 _ ->
-                     User_
-             end,
-    case statement_converter:decode(Raw) of
-        {ok, {<<"connect">>, Name}} ->
-            case ets:member(users, Name) of
-                true ->
-                    {reply,{<<"error">>, <<Name/binary, " is already used.">>} , State};
-                _ ->   
-                    ets:insert(users, #user{name=Name, pid=From, threads=[]}),
-                    {reply,{<<"welcome">>, Name} , State}
-            end;
-        {ok, {<<"quit">>}} ->
-            Name = User#user.name,
-            case ets:member(users, Name) of
-                true ->
-                    ets:delete(users, Name),
-                    {reply,{<<"goodbye">>, <<Name/binary>>} , State};
-                _ ->   
-                    {reply,{<<"error">>, <<Name/binary, " doesn't exist.">>} , State}
-            end;
-        {ok, {<<"join">>, Thread}} ->
-            case ets:member(threads, Thread) of
-                true ->
-                    ets:update_element(users, User#user.name, [{#user.threads, [Thread|User#user.threads]}]),
-                    {reply,{<<"welcome">>, <<"thread ", Thread/binary>>} , State};
-                _ ->   
-                    {ok, Pid} = supervisor:start_child(State#state.thread_sup, [Thread]),
-                    Ref = erlang:monitor(process, Pid),
-                    ets:update_element(users, User#user.name, [{#user.threads, [Thread|User#user.threads]}]),
-                    ets:insert(threads, #thread{name=Thread, pid=Pid, ref=Ref}),
-                    {reply,{<<"welcome">>, <<"thread ", Thread/binary, " has made.">>} , State}
-            end;
+handle_call({make_thread, Name}, _From, State) ->
+    case ets:member(threads, Name) of
+        true ->
+            {reply, {error, already_exists}, State};
         _ ->
-            {reply,{<<"error">>, <<"invalide statement.">>} , State}
+            case supervisor:start_child(State#state.thread_sup, [Name]) of
+                {ok, Pid} ->
+                    Ref = erlang:monitor(process, Pid),
+                    Thread = #thread{name=Name, pid=Pid, ref=Ref},
+                    ets:insert(threads, Thread),
+                    {reply, {ok, Thread}, State};
+                {error, Error} ->
+                    {reply, {error, Error}, State}
+            end
     end;
+
 handle_call(_Request, _From, State) ->
     {reply, ignored, State}.
 
--- apps/erlang_chat/src/ws_handler.erl
+++ apps/erlang_chat/src/ws_handler.erl
@@ -7,6 +7,10 @@
 -export([websocket_handle/2]).
 -export([websocket_info/2]).
 
+-record(state, {thread_sup}).
+-record(user, {name, pid, threads}).
+-record(thread, {name, pid, ref}).
+
 init(Req0, State) ->
 	NewValue = integer_to_list(rand:uniform(1000000)),
 	Req1 = cowboy_req:set_resp_cookie(<<"server">>, NewValue,
@@ -18,8 +22,8 @@ init(Req0, State) ->
 websocket_init(State) ->
     {reply, {text, << "connect." >>}, State, hibernate}.
 
-websocket_handle({text, Msg}, State) ->
-    Response = gen_server:call(chat_controller, {request, Msg}),
+websocket_handle({text, Msg}, State_) ->
+    {reply, Response, State} = handle_request(Msg, State_),
     {ok, Statement} = statement_converter:encode(Response),
     {reply, {text, Statement}, State};
 websocket_handle(_Data, State) ->
@@ -27,3 +31,60 @@ websocket_handle(_Data, State) ->
 
 websocket_info(_Info, State) ->
     {ok, State}.
+
+handle_request(Raw, State) ->
+    From = self(),
+    io:format(Raw),
+    User_ = ets:select(users, [{#user{pid=From, _='_'}, [], ['$_']}]),
+    [User] = case length(User_) of
+                 0 -> 
+                     [#user{name=""}];
+                 _ ->
+                     User_
+             end,
+    Statement = statement_converter:decode(Raw),
+    case User#user.name of
+        "" ->
+            case Statement of
+                {ok, {<<"connect">>, Name}} ->
+                    case ets:member(users, Name) of
+                        true ->
+                            {reply,{<<"error">>, <<Name/binary, " is already used.">>} , State};
+                        _ ->   
+                            ets:insert(users, #user{name=Name, pid=From, threads=[]}),
+                            {reply,{<<"welcome">>, Name} , State}
+                    end;
+                _ ->
+                    {reply, {<<"error">>, <<"connect first!">>}, State}
+            end;
+        _ ->
+            case Statement of
+                {ok, {<<"quit">>}} ->
+                    Name = User#user.name,
+                    case ets:member(users, Name) of
+                        true ->
+                            ets:delete(users, Name),
+                            {reply,{<<"goodbye">>, <<Name/binary>>} , State};
+                        _ ->   
+                            {reply,{<<"error">>, <<Name/binary, " doesn't exist.">>} , State}
+                    end;
+                {ok, {<<"join">>, Name}} ->
+                    case ets:member(threads, Name) of
+                        true ->
+                            [Thread] = ets:select(threads, [{#thread{name=Name, _='_'}, [], ['$_']}]),
+                            {reply,{<<"welcome">>, <<"thread ", Thread/binary>>} , State};
+                        _ ->   
+                            case gen_server:call(chat_controller, {make_thread, Name}) of
+                                {ok, Thread} ->
+                                    { reply
+                                    , {<<"welcome">>
+                                      , <<"thread ", (Thread#thread.name)/binary, " has made.">>} 
+                                    , State};
+                                {error, Error} ->
+                                    {reply, {<<"error">>, <<"can't make thread.">>}, State}
+                            end
+                    end;
+                _ ->
+                    {reply,{<<"error">>, <<"invalide statement.">>} , State}
+            end
+    end.
